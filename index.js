7220b5259bc8f26ae393d372134da11ea04d3f18
aj mac <macdonalda47@gmail.com>
2020-08-06 15:07:57 -0500

    most of functionality coded

--- index.js
+++ index.js
@@ -58,3 +58,20 @@ var connection = mysql.createConnection({
         
       })
     }
+
+
+    // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request. 
+//--> go see how many units there are of that item in the database.
+//--> does input of customer for the quantity match or fall below the quantity in the db
+
+
+
+
+
+//if false/not, console log "sorry! we only have (#) left."
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
